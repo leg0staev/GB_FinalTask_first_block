5c8ecf4a711f0a96d67018021252571092eaba3e
legostaev <alexey.legostaev@gmail.com>
2022-11-27 12:56:36 +0300

    the method of searching for all words consisting of less than three characters has been added

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@
                                     "in",
                                     "out"
 };
+int wordsQuantity = LetsFindQuantity(sourceArray);
+//test
+Console.WriteLine($"количество элементов в исходном масииве => {wordsQuantity}");
 
 // найду количество слов меньше 3 символов
-int LetsFind(string[] array){
+int LetsFindQuantity(string[] array){
     int quantity = 0;
     for (int i = 0; i < array.Length; i++)
     {
@@ -18,5 +21,14 @@ int LetsFind(string[] array){
     return quantity;
 };
 
-//test
-Console.WriteLine($"количество элементов в исходном масииве => {LetsFind(sourceArray)}");
+//выберу все слова, которые удовлетворяют условию в новый массив
+string[] LetsFindWords(string[] array, int newArrayLength){
+    string[] resultArray = new string[newArrayLength];
+    for (int i = 0; i < array.Length; i++){
+        int newIndex = 0;
+        if (array[i].Length <= 3) resultArray[newIndex] = array[i];
+        newIndex++;
+    }
+    return resultArray;
+}
+
