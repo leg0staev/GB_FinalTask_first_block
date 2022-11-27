2e81e0021b2abec8444ecde4552496d5a66b7ca4
legostaev <alexey.legostaev@gmail.com>
2022-11-27 17:30:39 +0300

    the ability to request slow from the user has been added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿string[] sourceArray = new string[7] {
-                                    "I",
-                                    "category",
-                                    "love",
-                                    "genetic",
-                                    "c#",
-                                    "outside",
-                                    "included"                                    
-};
+﻿// string[] sourceArray = new string[7] {
+//                                     "I",
+//                                     "category",
+//                                     "love",
+//                                     "genetic",
+//                                     "c#",
+//                                     "outside",
+//                                     "included"                                    
+// };
+
+
+
+string[] sourceArray = GetSourceArray("Введите слова для анализа через пробел: ");
+
 int wordsQuantity = LetsFindQuantity(sourceArray);
 string[] ResultArray = LetsFindWords(sourceArray, wordsQuantity);
 //test
@@ -21,7 +26,7 @@ int LetsFindQuantity(string[] array){
         if (array[i].Length <=4) quantity++;
     }
     return quantity;
-};
+}
 
 //выберу все слова, которые удовлетворяют условию в новый массив
 string[] LetsFindWords(string[] array, int newArrayLength){
@@ -40,7 +45,15 @@ string[] LetsFindWords(string[] array, int newArrayLength){
 void PrintArray(string[] array){
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]}, ");
+        Console.Write($"{array[i]} ");
     }
-};
+}
 
+//вывод запроса на ввод слов от пользователя
+string[] GetSourceArray(string systemMessage){
+    Console.Write(systemMessage);
+
+    string userInput = Console.ReadLine() ?? string.Empty;
+    string[] sourceArray = userInput.Split(" ");
+    return sourceArray;
+}
