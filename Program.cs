0230cb295df311fc218290161b63a70092e66f44
legostaev <alexey.legostaev@gmail.com>
2022-11-27 15:21:02 +0300

    bug fix

--- Program.cs
+++ Program.cs
@@ -1,22 +1,24 @@
 ﻿string[] sourceArray = new string[7] {
-                                    "to",
-                                    "the",
-                                    "love",
+                                    "I",
                                     "category",
-                                    "task",
-                                    "in",
-                                    "out"
+                                    "love",
+                                    "genetic",
+                                    "c#",
+                                    "outside",
+                                    "included"                                    
 };
 int wordsQuantity = LetsFindQuantity(sourceArray);
+string[] ResultArray = LetsFindWords(sourceArray, wordsQuantity);
 //test
 Console.WriteLine($"количество элементов в исходном масииве => {wordsQuantity}");
+PrintArray(ResultArray);
 
 // найду количество слов меньше 3 символов
 int LetsFindQuantity(string[] array){
     int quantity = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <=3) quantity++;
+        if (array[i].Length <=4) quantity++;
     }
     return quantity;
 };
@@ -24,19 +26,21 @@ int LetsFindQuantity(string[] array){
 //выберу все слова, которые удовлетворяют условию в новый массив
 string[] LetsFindWords(string[] array, int newArrayLength){
     string[] resultArray = new string[newArrayLength];
+    int newIndex = 0;
     for (int i = 0; i < array.Length; i++){
-        int newIndex = 0;
-        if (array[i].Length <= 3) resultArray[newIndex] = array[i];
-        newIndex++;
+        if (array[i].Length <= 4){
+            resultArray[newIndex] = array[i];
+            newIndex++;
+        }
     }
     return resultArray;
 }
 
-//метод вывода массива в консоль
+// метод вывода массива в консоль
 void PrintArray(string[] array){
     for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine($"{array[i]}, ");
+        Console.Write($"{array[i]}, ");
     }
 };
 
