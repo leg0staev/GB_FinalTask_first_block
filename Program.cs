cc9018dde3c3262f2f49e210418a460ba0731422
legostaev <alexey.legostaev@gmail.com>
2022-11-27 12:18:39 +0300

    method of searching for words less than three letters has been added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] sourceArray = new string[7] {
+                                    "to",
+                                    "the",
+                                    "love",
+                                    "category",
+                                    "task",
+                                    "in",
+                                    "out"
+};
+
+// найду количество слов меньше 3 символов
+int LetsFind(string[] array){
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <=3) quantity++;
+    }
+    return quantity;
+};
+
+//test
+Console.WriteLine($"количество элементов в исходном масииве => {LetsFind(sourceArray)}");
